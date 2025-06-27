5bd7e6387864ac8d40299b23ff95b872d2c08303
marcodialvva <marcodialvva@gmail.com>
2025-06-27 11:05:58 -0600

    Refactor(IAgenda): update return types for findClassByName and findClassByDay methods to include null
    - Changed the return type of findClassByName and findClassByDay methods from FindClassByDayResult to FindClassByDayResult | null.
    - This modification enhances type safety and clarity, allowing for better handling of cases when no class is found.

--- agenda/IAgenda.ts
+++ agenda/IAgenda.ts
@@ -1,12 +1,14 @@
+import { FindClassByDayResult } from './helpers';
+
 export class Class {
     constructor(public name: string, public day: string, public hour: number) { }
 }
 
 export interface IAgenda {
-    bookClass(name: string, day: string, hour: number): void;
-    deleteClass(day: string, hour: number): void;
-    moveClass(dayOrigin: string, hourOrigin: number, dayDestination: string, hourDestination: number): void;
-    findClassByName(name: string): (Class[] | null);
-    findClassByDay(day: string, hour: number): void;
+    bookClass(name: string, day: string, hour: number): string;
+    deleteClass(day: string, hour: number): string;
+    moveClass(dayOrigin: string, hourOrigin: number, dayDestination: string, hourDestination: number): string;
+    findClassByName(name: string): FindClassByDayResult | null;
+    findClassByDay(day: string, hour: number): FindClassByDayResult | null;
 
 }
